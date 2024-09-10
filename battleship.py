5cde8ba8dc96b8dbf11038c6af9a433f68ab0355
AMcFerrin <andrew.mcferrin1@gmail.com>
2024-09-10 17:43:55 -0500

    started working on Battleship

--- battleship.py
+++ battleship.py
@@ -1,3 +1,19 @@
-class Battleship:
+from board import Board
 
+class Battleship:
+     def __init__(self, turn):
+          self.boardZero = Board
+          self.boardOne = Board
+          self.turn = turn
+
+    def takeTurn(self, turn):
+     
+    
+    def _switchTurn(self):
+         
+          
+    
+    def checkWin(self):
+         if Board.allSunk:
+              return True
     
