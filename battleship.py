a9eddb5fa233768f2287972408689ea84529d767
AMcFerrin <andrew.mcferrin1@gmail.com>
2024-09-11 18:45:10 -0500

    Worked on rest of the methods

--- battleship.py
+++ battleship.py
@@ -1,19 +1,44 @@
 from board import Board
 
 class Battleship:
-     def __init__(self, turn):
-          self.boardZero = Board
-          self.boardOne = Board
-          self.turn = turn
-
-    def takeTurn(self, turn):
-     
-    
-    def _switchTurn(self):
-         
+    def __init__(self, Board, shipList1, shipList2):
+          self.boardZero = Board(shipList1)
+          self.boardOne = Board(shipList2)
+          self.turn = 0
           
-    
-    def checkWin(self):
+          self.curBoard = self.boardZero
+
+
+    def takeTurn(self, coords):
+        if Board.shoot(self.curBoard, coords) == True:
+            if self._checkWin == True:
+                if self.turn == 0:
+                    return 0
+                else: 
+                    return 1
+            else: 
+                if self.turn == 0:
+                    self._switchTurn
+                    return 2
+                else:
+                    self._switchTurn
+                    return 2
+        else:
+            return 3
+
+        
+    def _switchTurn(self):
+        if turn == 0:
+            self.curBoard = self.boardOne
+            turn = 1
+        else:
+            self.curBoard = self.boarZero
+            turn = 0
+ 
+          
+    def _checkWin(self):
          if Board.allSunk:
-              return True
+            return True
+         else:
+            return False
     
