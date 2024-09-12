5ee414380d6d97861d84433bda4c9b8cfb91a3e4
LandonPyko <pykomar2@gmail.com>
2024-09-11 23:17:07 -0500

    made the square pixels scalable off of screen size

--- test.py
+++ test.py
@@ -3,7 +3,15 @@ import pygame as pg
 pg.init()
 
 running = True
-screen =  pg.display.set_mode((1000,1000))
+x = 1000
+y = 1000
+single_square_x = x / 10
+single_square_y = y / 10
+
+max_x = x - single_square_x
+max_y = y - single_square_y
+
+screen =  pg.display.set_mode((x,y))
 rect = pg.Rect((0,0), (100,100))
 
 # need to blit() if using an image
@@ -18,28 +26,28 @@ while running:
     for event in events:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_RIGHT:
-                if rect.x < 900:
-                    rect.move_ip(100,0)
+                if rect.x < max_x:
+                    rect.move_ip(single_square_x,0)
                 else:
                     rect.x = 0
 
             elif event.key == pg.K_LEFT:
 
                 if rect.x > 0:
-                    rect.move_ip(-100,0)
+                    rect.move_ip(-(single_square_x),0)
                 else:
-                    rect.x = 900
+                    rect.x = max_x
             
             elif event.key == pg.K_DOWN:
 
-                if rect.y < 900:               
-                    rect.move_ip(0,100)
+                if rect.y < max_y:               
+                    rect.move_ip(0,single_square_y)
                 else:
                     rect.y = 0
 
             elif event.key == pg.K_UP:
 
                 if rect.y > 0:
-                    rect.move_ip(0,-100)
+                    rect.move_ip(0,-(single_square_y))
                 else:
-                    rect.y = 900
+                    rect.y = max_y
