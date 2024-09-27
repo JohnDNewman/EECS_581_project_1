207df4c3063ce62e663f429a8f0b92a06165d85f
jwund <jwunderlich1127@gmail.com>
2024-09-26 23:25:01 -0500

    added ai battleship file

---
+++ aibattleship.py
@@ -0,0 +1,26 @@
+'''
+EECS 581 Project 2
+Description: 
+Inputs: 
+Outputs: 
+Authors: Jackson Wunderlich, Dylan Sailors, Ginny Ke
+Creation Date: 9-26-2024
+Last Modified: 9-26-2024
+Sources: 
+'''
+
+class BattleshipAI:
+    def random_place_ship(self):
+        pass
+
+    def ai_turn(self):
+        pass
+
+    def ai_easy_turn(self):
+        pass
+
+    def ai_medium_turn(self):
+        pass
+
+    def ai_hard_turn(self):
+        pass
