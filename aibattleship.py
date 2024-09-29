af3405dc060c126726d45d31e8a91a5f7b541a8d
Ginny Ke <130810135+Ginnyk10@users.noreply.github.com>
2024-09-28 20:14:31 -0500

    Created randomPlaceShip Function

--- aibattleship.py
+++ aibattleship.py
@@ -9,10 +9,33 @@ Last Modified: 9-26-2024
 Sources: 
 '''
 
+import random
 class BattleshipAI:
-    def randomPlaceShip(self):
-        pass
+    def __init__ (self, board): #initialize battleship board 
+        self.board = board 
 
+    def randomPlaceShip(self):
+        for ship in self.board.shipList:
+            placed = False
+            while not placed:
+                orientation = random.choice(['horizontal', 'vertical'])  # Randomly choose orientation
+
+                # Choose random row and column within bounds to fit the ship
+                if orientation == 'horizontal':
+                    row = random.randint(0, 9)
+                    col = random.randint(0, 9 - ship.size)
+                    ship_coordinates = [(row, col + i) for i in range(ship.size)]
+                else:
+                    row = random.randint(0, 9 - ship.size)
+                    col = random.randint(0, 9)
+                    ship_coordinates = [(row + i, col) for i in range(ship.size)]
+
+                # Check if all ship coordinates are valid
+                if all(self.board.coordsMatrix[coord[0]][coord[1]] == 0 for coord in ship_coordinates):
+                    ship.coords = ship_coordinates
+                    for coord in ship_coordinates:
+                        self.board.coordsMatrix[coord[0]][coord[1]] = 2  # Mark as taken
+                    placed = True
     def aiTurn(self):
         pass
 
